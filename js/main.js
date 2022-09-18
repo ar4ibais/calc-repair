70b97944c9c9cdbf66a1189cc046440f69e743f3
ar4ibais <ak.hutov@yandex.ru>
2022-09-18 15:31:11 +0400

    made a refactoring and made a big function for work with inputs and checkboxes

--- js/main.js
+++ js/main.js
@@ -1,26 +1,70 @@
 // Элементы формы
 const squareInput = document.querySelector('#square-input'),
       squareRange = document.querySelector('#square-range'),
-      totalPrice = document.querySelector('#total-price'),
-      basePrice = 6000;
+      basePrice = 6000,
+      totalPriceElement = document.querySelector('#total-price'),
+      inputs = document.querySelectorAll('input'),
+      radiosType = document.querySelectorAll('input[name="type"]'),
+      radioBuild = document.querySelectorAll('input[name="building"]'),
+      radioRooms = document.querySelectorAll('input[name="rooms"]'),
+      ceilings = document.querySelector('input[name="ceiling"]'),
+      walls = document.querySelector('input[name="walls"]'),
+      floor = document.querySelector('input[name="floor"]');
 
 // Связка ползунка с тексовым значением
 squareRange.addEventListener('input', () => {
     squareInput.value = squareRange.value;
-    setCost();
 });
 
 squareInput.addEventListener('input', () => {
     squareRange.value = squareInput.value;
-    setCost();
 });
 
-//Указываем финальную стоимость без всяких опций
-function setCost() {
-    const formatter = new Intl.NumberFormat('ru-RU');
-    let price = parseInt(squareInput.value) * basePrice;
+//Указываем финальную стоимость
+function calculate() {
+    let totalPrice = basePrice * parseInt(squareInput.value);
 
-    totalPrice.innerText = formatter.format(price);
+    for (let radio of radiosType) {
+        if (radio.checked) {
+            totalPrice = totalPrice * parseFloat(radio.value);
+        }
+    }
+
+    for (let radio of radioBuild) {
+        if (radio.checked) {
+            totalPrice = totalPrice * parseFloat(radio.value);
+        }
+    }
+
+    for (let radio of radioRooms) {
+        if (radio.checked) {
+            totalPrice = totalPrice * parseFloat(radio.value);
+        }
+    }
+
+    if (ceilings.checked) {
+        totalPrice *= parseFloat(ceilings.value);
+    }
+
+    if (walls.checked) {
+        totalPrice *= parseFloat(walls.value);
+    }
+
+    if (floor.checked) {
+        totalPrice *= parseFloat(floor.value);
+    }
+    
+    const formatter = new Intl.NumberFormat('ru');
+
+    totalPriceElement.textContent = formatter.format(totalPrice);
 }
 
-setCost();
+calculate();
+
+//Навешиваем функцию расчета стоимости для каждого инпута при изменении
+
+for (let input of inputs) {
+    input.addEventListener('input', () => {
+        calculate();
+    });
+}
